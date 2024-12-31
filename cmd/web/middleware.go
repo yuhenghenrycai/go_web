df80f7777f882179423bf44f6ad9e0574647b0c6
Yuheng Cai <yuhengcai@192.168.10.13>
2024-12-31 16:33:39 +0800

    load and save session on every request

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -14,14 +14,20 @@ func WriteToConsole(next http.Handler) http.Handler {
 	})
 }
 
+// NoSurf adds CSRF protection to all POST request
 func NoSurf(next http.Handler) http.Handler {
 	csrfHandler := nosurf.New(next)
 	// set CSRF token for every incoming request
 	csrfHandler.SetBaseCookie(http.Cookie{
 		HttpOnly: true,
 		Path:     "/",
-		Secure:   false,
+		Secure:   app.InProduction,
 		SameSite: http.SameSiteLaxMode,
 	})
 	return csrfHandler
+}
+
+// Load and save session for every request
+func SessionLoad(next http.Handler) http.Handler {
+	return session.LoadAndSave(next)
 }
--- go.mod
+++ go.mod
@@ -5,3 +5,5 @@ go 1.23.2
 require github.com/go-chi/chi v1.5.5
 
 require github.com/justinas/nosurf v1.1.1
+
+require github.com/alexedwards/scs/v2 v2.8.0 // indirect
