87648a99ed0d45403338b71965a6c0517d7aeb2d
Yuheng Cai <yuhengcai@Yuhengs-MacBook-Pro.local>
2024-12-31 15:24:47 +0800

    replace pat router with chi router; implement CSRF token generation logic in router middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,16 +3,20 @@ package main
 import (
 	"net/http"
 
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 	"github.com/yuhenghenrycai/go_web/pkg/config"
 	"github.com/yuhenghenrycai/go_web/pkg/handlers"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
+	mux := chi.NewRouter()
 
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	mux.Use(middleware.Recoverer)
+	mux.Use(NoSurf)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 
 	return mux
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,6 @@ module github.com/yuhenghenrycai/go_web
 
 go 1.23.2
 
-require github.com/bmizerany/pat v0.0.0-20210406213842-e4b6760bdd6f // indirect
+require github.com/go-chi/chi v1.5.5
+
+require github.com/justinas/nosurf v1.1.1
